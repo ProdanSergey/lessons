387eed02d891078724da34eff52cd65c4e0a75b0
Serhii Prodan <prodan.sergey.od@gmail.com>
2021-08-15 13:47:05 +0300

    Vue ToDo list

---
+++ vue-todo-list/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<main>
+		<div class="container">
+			<TodoList/>
+		</div>
+	</main>
+</template>
+
+<script>
+import TodoList from './components/List.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+	.container {
+		margin: 0 auto;
+		max-width: 720px;
+	}
+</style>
