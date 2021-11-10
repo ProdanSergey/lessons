1e1ba91b497be0c1a8f2805611387dcd981207d0
ProdanSergey <prodan.sergey.od@gmail.com>
2021-11-10 10:40:23 +0200

    classwork(13): DOM, Selectors, Traverse

---
+++ 13 - DOM, Selectors, Traverse/classwork/app/index.js
@@ -0,0 +1,52 @@
+// html
+// console.dir(document.documentElement);
+
+// body 
+// console.dir(document.body);
+
+// const body = document.body;
+
+// head 
+// console.dir(document.head);
+
+// const headline = document.getElementById('headline');
+
+// console.dir(headline);
+
+// const allHeaders = document.getElementsByTagName('header');
+
+// console.dir(allHeaders);
+
+// console.dir(allHeaders[0]);
+
+// const htmlCollection = document.getElementsByClassName('card');
+
+// console.dir(htmlCollection);
+
+// const nodeList = document.querySelectorAll('.card');
+
+// console.dir(nodeList);
+
+// // Delete last card;
+// nodeList[nodeList.length - 1].remove();
+
+// console.dir(htmlCollection);
+// console.dir(nodeList);
+
+// const uniqCard = document.body.querySelector('#uniq');
+
+// const primaryCard = document.body.querySelector('#primary');
+
+// console.log(uniqCard.contains(uniqCard));
+
+// console.log(uniqCard.innerHTML = '<script>console.log("HACKED")</script>');
+
+// console.log(uniqCard.outerText = '<p>Hello World<p>');
+
+// console.log(uniqCard);
+
+// const doHack = () => {
+// 	console.log('HACKED!');
+// };
+
+// uniqCard.innerHTML = '<button onclick="doHack()">Get Insta Money</button>';
