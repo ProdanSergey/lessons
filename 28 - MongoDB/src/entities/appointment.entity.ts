6703980cf38b729372de424fed7f8e99adb31466
ProdanSergey <prodan.sergey.od@gmail.com>
2022-01-11 10:29:11 +0100

    refactoring(): Reorganizing and renaming file tree

--- 28 - MongoDB/src/entities/appointment.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Entity } from "../services/entity.service";
-import { Doctor } from "./doctor.entity";
-
-export class Appointment extends Entity {
-	startAt?: string;
-	userId?: string;
-	doctor?: Doctor;
-}
